03be3c6af7abb038415bf45ff30ef0bb0b0e3c41
c70 <c70sp@proton.me>
2024-11-14 14:39:07 +0100

    Added a few helper functions

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,26 @@
 function distance(p1, p2){
     return Math.hypot(p1.x - p2.x, p1.y - p2.y);
 }
+
+function getAzimuth(p1, p2){
+    return Math.atan2(-(p1.x - p2.x), p1.y - p2.y);
+    // return Math.atan2(p2.x - p1.x, p2.y - p1.y);
+}
+
+function degToRad(deg){
+    return deg * (Math.PI / 180)
+}
+
+function radToDeg(rad){
+    return rad * (180 / Math.PI);
+}
+
+function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
